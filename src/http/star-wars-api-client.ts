cbb4122403b3a5ce2430f9b748fb113cb9f716b3
Brandon <blenz@expapp.com>
2020-11-02 17:52:27 -0500

    Create basic star wars api wrapper with expected types

---
+++ src/http/star-wars-api-client.ts
@@ -0,0 +1,39 @@
+import HttpClient from "./http-client"
+
+export interface PlanetData {
+    name: string
+    diameter: string
+    rotation_period: string
+    orbital_period: string
+    gravity: string
+    population: string
+    climate: string
+    terrain: string
+    surface_water: string
+    residents: Array<string>
+    films: Array<string>
+    url: string
+    created: string
+    edited: string
+}
+
+interface PlanetsData {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: Array<PlanetData>
+}
+
+class StarWarsApiClient extends HttpClient {
+
+    private static BASE_URL = 'https://swapi.dev/api/';
+    private static PLANETS = 'planets';
+
+    constructor() {
+        super(StarWarsApiClient.BASE_URL);
+    }
+
+    getPlanetsPage = (pageUrl: string) => {
+        return this.instance.get<PlanetsData>(pageUrl);
+    };
+}
