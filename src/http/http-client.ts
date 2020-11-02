5ae386d6b17709b7c4ffc34dd5bb9c40c0f1f48e
Brandon <blenz@expapp.com>
2020-11-02 17:38:15 -0500

    Add basic http client

---
+++ src/http/http-client.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosInstance } from 'axios'
+
+const DEBUG: boolean = true; // TODO: application property and set to false by default
+
+abstract class HttpClient {
+    protected readonly instance: AxiosInstance;
+
+    protected constructor(baseURL: string) {
+        this.instance = axios.create({
+            baseURL
+        });
+
+        this.instance.interceptors.request.use(
+            value => {
+                DEBUG && console.log(value);
+                return value;
+            }, error => {
+                DEBUG && console.log(error);
+                return Promise.reject(error);
+            }
+        );
+
+        this.instance.interceptors.response.use(
+            response => {
+                DEBUG && console.log(response);
+                return response;
+            }, error => {
+                DEBUG && console.log(error);
+                return Promise.reject(error);
+            }
+        );
+    }
+}
+
+export default HttpClient;
